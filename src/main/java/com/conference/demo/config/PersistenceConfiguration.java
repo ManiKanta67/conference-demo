a366f70eec0ded7c3a85e100dedcf29124e68367
mania <mania@mania-Lenovo-G580>
2021-11-14 00:40:44 +0530

    Configuring the data source using java code

---
+++ src/main/java/com/conference/demo/config/PersistenceConfiguration.java
@@ -0,0 +1,25 @@
+package com.conference.demo.config;
+
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PersistenceConfiguration {
+
+	@Bean
+	public DataSource dataSource()
+	{
+		DataSourceBuilder builder = DataSourceBuilder.create();
+		builder.url("jdbc:mysql://localhost:3306/conference_demo");
+		/*
+		builder.username("user name");
+		builder.password("password");
+		*/
+		System.out.println("My custom data source bean has been initialized and set");
+		return builder.build();
+	}
+}
