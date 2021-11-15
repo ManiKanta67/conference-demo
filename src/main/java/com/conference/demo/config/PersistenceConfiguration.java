4038d41169e3387ba4911dfb542d23cc99c75df9
mania <mania@mania-Lenovo-G580>
2021-11-15 09:03:53 +0530

    Remove java configuration as it'd break the connection property in properties file.

--- src/main/java/com/conference/demo/config/PersistenceConfiguration.java
+++ src/main/java/com/conference/demo/config/PersistenceConfiguration.java
@@ -1,6 +1,5 @@
 package com.conference.demo.config;
 
-
 import javax.sql.DataSource;
 
 import org.springframework.boot.jdbc.DataSourceBuilder;
@@ -10,16 +9,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class PersistenceConfiguration {
 
-	@Bean
-	public DataSource dataSource()
-	{
-		DataSourceBuilder builder = DataSourceBuilder.create();
-		builder.url("jdbc:mysql://localhost:3306/conference_demo");
-		/*
-		builder.username("user name");
-		builder.password("password");
-		*/
-		System.out.println("My custom data source bean has been initialized and set");
-		return builder.build();
-	}
+	/*
+	 * @Bean public DataSource dataSource() { DataSourceBuilder builder =
+	 * DataSourceBuilder.create();
+	 * builder.url("jdbc:mysql://localhost:3306/conference_demo");
+	 * builder.username("user name"); builder.password("password");
+	 * System.out.println("My custom data source bean has been initialized and set"
+	 * ); return builder.build(); }
+	 */
 }
